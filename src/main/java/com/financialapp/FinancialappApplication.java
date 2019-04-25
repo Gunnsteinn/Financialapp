d2def11c696a9406f80e5a9978b1c6ef670044d5
Xprox <guillecolo@gmail.com>
2019-04-25 19:29:02 -0300

    first commit

---
+++ src/main/java/com/financialapp/FinancialappApplication.java
@@ -0,0 +1,13 @@
+package com.financialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financialapp/FinancialappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.financialapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinancialappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

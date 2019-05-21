700e4912f3c545adb9aef14be1906e75ad1e1b43
Xprox <guillecolo@gmail.com>
2019-05-21 00:31:56 -0300

    Add Thread Pool.

--- src/main/java/com/financialapp/service/CurrencyServiceImp.java
+++ src/main/java/com/financialapp/service/CurrencyServiceImp.java
@@ -3,11 +3,15 @@ package com.financialapp.service;
 import com.financialapp.model.Bank;
 import com.financialapp.model.Currency;
 import com.financialapp.repository.CurrencyRepository;
+import com.google.common.util.concurrent.MoreExecutors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service("currencyService")
 public class CurrencyServiceImp implements CurrencyService{
@@ -28,8 +32,34 @@ public class CurrencyServiceImp implements CurrencyService{
         }
     }
 
+    List<Callable<List<Currency>>> callables = Arrays.asList(
+            () -> findParticularCurrency("BS"),
+            () -> findParticularCurrency("BP"),
+            () -> findParticularCurrency("BF"),
+            () -> findParticularCurrency("BN"),
+            () -> findParticularCurrency("BG"),
+            () -> findParticularCurrency("BI"));
+
+
     public List<Bank> findAllCurrency() {
         try {
+            ExecutorService executor = Executors.newWorkStealingPool();
+            executor.invokeAll(callables)
+                    .stream()
+                    .map(future -> {
+                        try {
+                            List<Currency> pepe =  future.get();
+                            System.out.println("pepepepepepe: " + pepe);
+                            return pepe;
+                        }
+                        catch (Exception e) {
+                            throw new IllegalStateException(e);
+                        }
+                    })
+                    .forEach(item ->  System.out.println("Finish: " + item));
+
+            System.out.println("Finish: " + allCurrencies);
+                    Long start = System.currentTimeMillis()/1000;
             List<Bank> allCurrencies = new ArrayList<Bank>();
             List<Currency> currenciesBS = findParticularCurrency("BS");
             List<Currency> currenciesBP = findParticularCurrency("BP");
@@ -45,6 +75,7 @@ public class CurrencyServiceImp implements CurrencyService{
             allCurrencies.add(new Bank("BBVA Francés",currenciesBF));
             allCurrencies.add(new Bank("Banco Galicia",currenciesBG));
             allCurrencies.add(new Bank("Banco ICBC",currenciesBI));
+            System.out.println("Finish: " + (System.currentTimeMillis()/1000-start));
 
             return allCurrencies;
         }catch (Exception e){
@@ -62,5 +93,7 @@ public class CurrencyServiceImp implements CurrencyService{
             return allCurrencies;
         }
     }
+
+
 
 }

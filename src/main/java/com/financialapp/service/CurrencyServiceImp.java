fad68438fd528ac2aa3f88c17a292dff0399115c
Xprox <guillecolo@gmail.com>
2019-05-21 00:50:13 -0300

    make it better

--- src/main/java/com/financialapp/service/CurrencyServiceImp.java
+++ src/main/java/com/financialapp/service/CurrencyServiceImp.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collector;
 
 @Service("currencyService")
 public class CurrencyServiceImp implements CurrencyService{
@@ -33,49 +34,47 @@ public class CurrencyServiceImp implements CurrencyService{
     }
 
     List<Callable<List<Currency>>> callables = Arrays.asList(
-            () -> findParticularCurrency("BS"),
             () -> findParticularCurrency("BP"),
             () -> findParticularCurrency("BF"),
             () -> findParticularCurrency("BN"),
             () -> findParticularCurrency("BG"),
-            () -> findParticularCurrency("BI"));
+            () -> findParticularCurrency("BI"),
+            () -> findParticularCurrency("BS")
+    );
 
 
     public List<Bank> findAllCurrency() {
         try {
+            Long start = System.currentTimeMillis()/1000;
+            List<Bank> allCurrencies = new ArrayList<Bank>();
             ExecutorService executor = Executors.newWorkStealingPool();
             executor.invokeAll(callables)
                     .stream()
                     .map(future -> {
                         try {
-                            List<Currency> pepe =  future.get();
-                            System.out.println("pepepepepepe: " + pepe);
-                            return pepe;
+                            return future.get();
                         }
                         catch (Exception e) {
                             throw new IllegalStateException(e);
                         }
                     })
-                    .forEach(item ->  System.out.println("Finish: " + item));
-
-            System.out.println("Finish: " + allCurrencies);
-                    Long start = System.currentTimeMillis()/1000;
-            List<Bank> allCurrencies = new ArrayList<Bank>();
-            List<Currency> currenciesBS = findParticularCurrency("BS");
-            List<Currency> currenciesBP = findParticularCurrency("BP");
-            List<Currency> currenciesBF = findParticularCurrency("BF");
-            List<Currency> currenciesBN = findParticularCurrency("BN");
-            List<Currency> currenciesBG = findParticularCurrency("BG");
-            List<Currency> currenciesBI = findParticularCurrency("BI");
-
-
-            allCurrencies.add(new Bank("Santander Rio",currenciesBS));
-            allCurrencies.add(new Bank("Banco de la Nación Argentina",currenciesBN));
-            allCurrencies.add(new Bank("Banco Patagonia",currenciesBP));
-            allCurrencies.add(new Bank("BBVA Francés",currenciesBF));
-            allCurrencies.add(new Bank("Banco Galicia",currenciesBG));
-            allCurrencies.add(new Bank("Banco ICBC",currenciesBI));
+                    .forEach(item -> allCurrencies.add(new Bank("Banco",item)));
             System.out.println("Finish: " + (System.currentTimeMillis()/1000-start));
+
+//            List<Currency> currenciesBS = findParticularCurrency("BS");
+//            List<Currency> currenciesBP = findParticularCurrency("BP");
+//            List<Currency> currenciesBF = findParticularCurrency("BF");
+//            List<Currency> currenciesBN = findParticularCurrency("BN");
+//            List<Currency> currenciesBG = findParticularCurrency("BG");
+//            List<Currency> currenciesBI = findParticularCurrency("BI");
+//
+//
+//            allCurrencies.add(new Bank("Santander Rio",currenciesBS));
+//            allCurrencies.add(new Bank("Banco de la Nación Argentina",currenciesBN));
+//            allCurrencies.add(new Bank("Banco Patagonia",currenciesBP));
+//            allCurrencies.add(new Bank("BBVA Francés",currenciesBF));
+//            allCurrencies.add(new Bank("Banco Galicia",currenciesBG));
+//            allCurrencies.add(new Bank("Banco ICBC",currenciesBI));
 
             return allCurrencies;
         }catch (Exception e){

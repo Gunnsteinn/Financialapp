96450ebc5fb9fe260edadcc7ba9e89a361074a8d
Guillermo Colombero <guillecolo@gmail.com>
2019-05-24 17:09:14 -0300

    Update StringUtils.java

--- src/main/java/com/financialapp/util/StringUtils.java
+++ src/main/java/com/financialapp/util/StringUtils.java
@@ -2,26 +2,27 @@ package com.financialapp.util;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StringUtils{
 
-//    @Value("#{'${type.currency.list}'.split(',')}")
-//    private static List<String> typeOfCurrencyList;
-//
-//    private static List<String> list = Arrays.asList(new String[]{"DOLAR", "EURO", "REAL", "URUGUAYO"});
-//
-//    public static String stringNormalize(String s) {
-//        for(String temp : list){
-//            Pattern pat = Pattern.compile(temp);
-//            Matcher mat = pat.matcher(Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").toUpperCase());
-//
-//            if (mat.find()){
-//               s = mat.group();
-//            break;
-//           }
-//        }
-//        return s;
-//    }
+    @Value("#{'${type.currency.list}'.split(',')}")
+    private static List<String> typeOfCurrencyList;
+
+    public static String stringNormalize(String s) {
+        for(String temp : list){
+            Pattern pat = Pattern.compile(temp);
+            Matcher mat = pat.matcher(Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "").toUpperCase());
+
+            if (mat.find()){
+               s = mat.group();
+            break;
+           }
+        }
+        return s;
+    }
 
     public static Double stringToDoubleNumber(String s) {
         Double num = 0.0;
